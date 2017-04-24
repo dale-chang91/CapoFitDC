deecf666f595f1a955f28fce10d2a1883725ee98
Dale Chang <dale.chang13@outlook.com>
2017-04-23 20:11:52 -0400

    Toolbar things

--- app/src/main/java/com/app/capofitdc/MainActivity.java
+++ app/src/main/java/com/app/capofitdc/MainActivity.java
@@ -2,23 +2,59 @@ package com.app.capofitdc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv = (ImageView) findViewById(R.id.main_image);
+        iv.setImageResource(R.drawable.capoeira);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
+        setSupportActionBar(toolbar);
     }
 
-    public void onButtonTap(View v) {
-        Toast myToast = Toast.makeText(getApplicationContext(), "Student created", Toast.LENGTH_LONG);
-            myToast.show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    public void createContact(View view) {
-        Intent intent = new Intent()
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) return true;
+
+        return super.onOptionsItemSelected(item);
     }
+    /*
+    private void initToolBars() {
+        Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbarTop);
+
+        Toolbar toolbarBottom = (Toolbar) findViewById(R.id.toolbar_bottom);
+        toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.:
+                        break;
+                }
+                return true;
+            }
+        });
+        toolbarBottom.inflateMenu(R.menu.menu_main);
+    }
+    */
 }
